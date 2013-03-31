2ad137e5f4c44743c3e5ccb3181d82af5d8de5b6
Saki Sato <sakiisato@gmail.com>
2013-03-31 14:21:48 +0900

    formatted for homepage

--- wp-content/themes/cleanslate/content-home.php
+++ wp-content/themes/cleanslate/content-home.php
@@ -7,13 +7,26 @@
  */
 ?>
 
-<?php if ( have_posts() ) :
-    
+<?php
+    if ( have_posts() ) :
+        
+        
+        
+        $current_year = date('Y', current_time('timestamp'));
+        $current_month = date('m', current_time('timestamp'));
+        $current_day = date('j', current_time('timestamp'));
+        
+        $args = array(
+            'cat'      => 2,
+            'year'     => $current_year,
+            'monthnum' => $current_month,
+            'day' => $current_day,
+            'order'    => 'ASC'
+        );
+        
+        query_posts( $args );
+        
         while ( have_posts() ) : the_post();
-            /* Include the Post-Format-specific template for the content.
-             * If you want to overload this in a child theme then include a file
-             * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-             */
             get_template_part( 'content', get_post_format() );
         endwhile;
     
