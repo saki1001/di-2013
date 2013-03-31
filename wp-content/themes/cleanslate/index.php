98eec4d3d69a7d7f9d018fdbecb51ae31eb94eeb
Saki Sato <sakiisato@gmail.com>
2013-03-31 18:11:21 +0900

    modified so no londer need content-home

--- wp-content/themes/cleanslate/index.php
+++ wp-content/themes/cleanslate/index.php
@@ -2,12 +2,6 @@
 /**
  * The main template file.
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
  * @package CleanSlate
  * @since CleanSlate 0.1
  */
@@ -17,7 +11,19 @@ get_header(); ?>
     <section id="content" role="main">
         
         <?php
-            get_template_part( 'content', 'home' );
+            if ( have_posts() ) :
+                
+                while ( have_posts() ) : the_post();
+                
+                    get_template_part( 'content', get_post_format() );
+                    
+                endwhile;
+                
+            else :
+            // Content Not Found Template
+            include('content-not-found.php');
+            
+            endif;
         ?>
     
     </section>
