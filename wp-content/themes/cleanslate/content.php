81383c9ca5b2881da991e51365d9fad49bacc452
Saki Sato <sakiisato@gmail.com>
2013-04-01 19:14:29 +0900

    added link to post title

--- wp-content/themes/cleanslate/content.php
+++ wp-content/themes/cleanslate/content.php
@@ -11,9 +11,7 @@
     
     <hgroup>
         <h2 class="post-title">
-            <?php
-                the_title();
-            ?>
+            <a href="<?php the_permalink(); ?>"><?php the_title();?></a>
         </h2>
         
         <?php
@@ -44,4 +42,4 @@
         <?php the_content(); ?>
     </div>
 
-</article><!-- #post-<?php the_ID(); ?> -->
+</article>
