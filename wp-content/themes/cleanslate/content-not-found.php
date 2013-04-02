5f36bbabd45f44be1cc2fe6e69728fd2a12212ee
Saki Sato <sakiisato@gmail.com>
2013-04-03 01:00:05 +0900

    removed section wrapper

--- wp-content/themes/cleanslate/content-not-found.php
+++ wp-content/themes/cleanslate/content-not-found.php
@@ -7,8 +7,4 @@
  */
 ?>
 
-<section id="content" role="main">
-    
-    <p class="not-found">Sorry, we can't find what you're looking for.</p>
-    
-</section>
+<p class="not-found">Sorry, we can't find what you're looking for.</p>
