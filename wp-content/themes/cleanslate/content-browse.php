7af584160d1a07377e44ede57d8e42df1894b9e5
Saki Sato <sakiisato@gmail.com>
2013-04-03 00:59:31 +0900

    took out browse filters, now it is included as a file from the category page

--- wp-content/themes/cleanslate/content-browse.php
+++ wp-content/themes/cleanslate/content-browse.php
@@ -6,47 +6,26 @@
  * @since CleanSlate 0.1
  */
 ?>
-    <div class="browse-filters">
-        <p>Filter by:</p>
+    
+<ul class="browse-posts">
+    <?php
+        $i = 0;
         
-        <ul class="tag-list">
-        <?php
-            $tags = get_tags();
-        
-            foreach ( $tags as $tag ) {
-                $tag_link = get_tag_link( $tag->term_id );
-        ?>
-            <li>
-                <a href="<?php echo $tag_link; ?>" title="<?php echo $tag->name; ?> Tag"><?php echo $tag->name; ?></a>
+        while ( have_posts() ) : the_post();
+            
+            if( $i % 4 == 0 ) :
+              $class = 'column first';
+            else :
+              $class = 'column';
+            endif;
+    ?>
+            <li class="<?php echo $class; ?>">
+                <?php
+                    get_template_part( 'content-thumb-square', get_post_format() );
+                ?>
             </li>
-        <?php
-            }
-        ?>
-        </ul>
-        
-        <?php get_search_form(); ?>
-        
-    </div>
-    
-    <ul class="browse-posts">
-<?php
-    $i = 0;
-    
-    while ( have_posts() ) : the_post();
-        
-        if( $i % 4 == 0 ) :
-          $class = 'column first';
-        else :
-          $class = 'column';
-        endif;
-?>
-        <li class="<?php echo $class; ?>">
-            <?php
-                get_template_part( 'content-thumb-square', get_post_format() );
-            ?>
-        </li>
-<?php
-    $i++;
-    endwhile;
-?>
-    </ul>
+    <?php
+        $i++;
+        endwhile;
+    ?>
+</ul>
