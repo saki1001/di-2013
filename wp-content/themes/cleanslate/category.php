4d9baedc29066ba41876896754b7d57de52df7cb
Saki Sato <sakiisato@gmail.com>
2013-04-03 00:58:42 +0900

    now check for which category in this page, also moved sidebar inside of content and added div around content-broswe template

--- wp-content/themes/cleanslate/category.php
+++ wp-content/themes/cleanslate/category.php
@@ -5,32 +5,55 @@
  * @package CleanSlate
  * @since CleanSlate 0.1
  */
+?>
 
-get_header(); ?>
-
-    <?php if ( have_posts() ) : ?>
-        
-        <section id="content" role="main">
-            
-            <?php while ( have_posts() ) : the_post(); ?>
-                
-                <?php
-                    /* Include the Post-Format-specific template for the content.
-                     * If you want to overload this in a child theme then include a file
-                     * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                     */
-                    get_template_part( 'content', get_post_format() );
-                ?>
-                
-            <?php endwhile; ?>
-            
-        </section>
-        
-    <?php else :
-        // Content Not Found Template
-        include('content-not-found.php');
-        
-    endif; ?>
+<?php get_header(); ?>
     
-<?php get_sidebar(); ?>
+    <section id="content" role="main">
+        
+        <?php
+            
+            $sidebar = FALSE;
+            
+            if ( have_posts() ) :
+                
+                if ( is_category('browse') ) :
+                    
+                    include('content-filters.php');
+                    get_template_part('content-browse', get_post_format() );
+                    
+                else :
+                    $sidebar = TRUE;
+            ?>
+                    
+                    <div id="articles">
+                    
+            <?php
+                    while ( have_posts() ) : the_post();
+                        get_template_part('content', get_post_format() );
+                    endwhile;
+            ?>
+                    
+                    </div>
+                    
+            <?php
+                endif;
+                
+            else :
+                $sidebar = TRUE;
+                
+                // Content Not Found Template
+                include('content-not-found.php');
+                
+            endif;
+        ?>
+        
+        <?php 
+            if ( $sidebar != FALSE) :
+                get_sidebar('featured');
+            endif;
+        ?>
+        
+    </section>
+    
 <?php get_footer(); ?>
