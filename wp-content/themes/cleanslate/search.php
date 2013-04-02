7aed6b72e9869abb3cd54147474194b4bd9cfe50
Saki Sato <sakiisato@gmail.com>
2013-04-03 01:11:50 +0900

    set up search to return results with  browse page template

--- wp-content/themes/cleanslate/search.php
+++ wp-content/themes/cleanslate/search.php
@@ -5,30 +5,27 @@
  * @package CleanSlate
  * @since CleanSlate 0.1
  */
+?>
 
- <?php
- /**
-  * The template for the Browse category.
-  *
-  * @package CleanSlate
-  * @since CleanSlate 0.1
-  */
- ?>
-
- <?php get_header(); ?>
+<?php get_header(); ?>
     
-     <section id="content" role="main">
-         
-         <?php
-             if ( have_posts() ) :
-                 get_template_part( 'content-browse', get_post_format() );
-             else :
-             // Content Not Found Template
-             include('content-not-found.php');
-             
-             endif;
-         ?>
-         
-     </section>
-     
- <?php get_footer(); ?>
+    <section id="content" role="main">
+        
+        <?php include('content-filters.php'); ?>
+        
+        <?php
+            
+            if ( have_posts() ) :
+                
+                get_template_part('content-browse', get_post_format() );
+                
+            else :
+                // Content Not Found Template
+                include('content-no-results.php');
+                
+            endif;
+        ?>
+        
+    </section>
+    
+<?php get_footer(); ?>
