b18d324976859139cc2a6836c26abfc0faa4f766
Saki Sato <sakiisato@gmail.com>
2013-04-03 23:12:15 +0900

    added back in the max_excerpt and get_category_tags functions

--- wp-content/themes/cleanslate/functions.php
+++ wp-content/themes/cleanslate/functions.php
@@ -40,6 +40,50 @@ function get_first_attachment() {
         return $link;
 }
 
+function the_excerpt_max_charlength($charlength) {
+    $excerpt = get_the_excerpt();
+    $charlength++;
+    
+    if ( mb_strlen( $excerpt ) > $charlength ) {
+        $subex = mb_substr( $excerpt, 0, $charlength - 5 );
+        $exwords = explode( ' ', $subex );
+        $excut = - ( mb_strlen( $exwords[ count( $exwords ) - 1 ] ) );
+        if ( $excut < 0 ) {
+            echo mb_substr( $subex, 0, $excut );
+        } else {
+            echo $subex;
+        }
+        echo '[...]';
+    } else {
+        echo $excerpt;
+    }
+}
+
+function get_category_tags($args) {
+    global $wpdb;
+    $tags = $wpdb->get_results
+    ("
+        SELECT DISTINCT terms2.term_id as tag_id, terms2.name as tag_name, null as tag_link
+        FROM
+            wp_posts as p1
+            LEFT JOIN wp_term_relationships as r1 ON p1.ID = r1.object_ID
+            LEFT JOIN wp_term_taxonomy as t1 ON r1.term_taxonomy_id = t1.term_taxonomy_id
+            LEFT JOIN wp_terms as terms1 ON t1.term_id = terms1.term_id,
+
+            wp_posts as p2
+            LEFT JOIN wp_term_relationships as r2 ON p2.ID = r2.object_ID
+            LEFT JOIN wp_term_taxonomy as t2 ON r2.term_taxonomy_id = t2.term_taxonomy_id
+            LEFT JOIN wp_terms as terms2 ON t2.term_id = terms2.term_id
+        WHERE
+            t1.taxonomy = 'category' AND p1.post_status = 'publish' AND terms1.term_id IN (".$args['categories'].") AND
+            t2.taxonomy = 'post_tag' AND p2.post_status = 'publish'
+            AND p1.ID = p2.ID
+        ORDER by tag_name
+    ");
+    
+    return $tags;
+}
+
 // Adding Thumbnails
 add_theme_support( 'post-thumbnails' );
 
