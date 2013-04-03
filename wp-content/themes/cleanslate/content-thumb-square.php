bd3044e3cf985a3527208bf5266152192b354b08
Saki Sato <sakiisato@gmail.com>
2013-04-03 23:10:16 +0900

    took out article wrapper, now included in while loop of content-browse

--- wp-content/themes/cleanslate/content-thumb-square.php
+++ wp-content/themes/cleanslate/content-thumb-square.php
@@ -7,59 +7,56 @@
  */
 ?>
 
-<!-- <article class="thumb"> -->
+
+<figcaption>
+    <h4 class="post-title">
+        <?php the_title(); ?>
+    </h4>
+    <p class="post-date">
+        <?php echo get_the_date(); ?>
+    </p>
+</figcaption>
+
+<figure class="post-thumb">
     
-    <figcaption>
-        <h4 class="post-title">
-            <?php the_title(); ?>
-        </h4>
-        <p class="post-date">
-            <?php echo get_the_date(); ?>
-        </p>
-    </figcaption>
-    
-    <figure class="post-thumb">
+    <?php
+        // BEGIN Get thumbnail
+        $args = array(
+            'post_parent' => $post->ID,
+            'post_type' => 'attachment',
+            'post_mime_type' => 'image',
+            'orderby' => 'menu_order',
+            'order' => 'ASC'
+        );
         
-        <?php
-            // BEGIN Get thumbnail
-            $args = array(
-                'post_parent' => $post->ID,
-                'post_type' => 'attachment',
-                'post_mime_type' => 'image',
-                'orderby' => 'menu_order',
-                'order' => 'ASC'
-            );
+        // Get image attachments
+        $attachments = get_children( $args );
+        
+        // Featured Image
+        $featImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID, 'browse-thumbnail') );
+        
+        if ( $attachments ) :
             
-            // Get image attachments
-            $attachments = get_children( $args );
-            
-            // Featured Image
-            $featImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID, 'browse-thumbnail') );
-            
-            if ( $attachments ) :
-                
-                if ( has_post_thumbnail($post->ID) ) :
-                    // Use Featured Image URL
-                    $thumb = $featImg;
-                    $thumbUrl = $thumb[0];
-                else :
-                    // Use only first value in array
-                    $attachment = array_shift( $attachments );
-                
-                    // Get thumbnail URL
-                    $thumb = wp_get_attachment_image_src( $attachment->ID, 'browse-thumbnail' );
-                    $thumbUrl = $thumb[0];
-                endif;
+            if ( has_post_thumbnail($post->ID) ) :
+                // Use Featured Image URL
+                $thumb = $featImg;
+                $thumbUrl = $thumb[0];
             else :
+                // Use only first value in array
+                $attachment = array_shift( $attachments );
             
-                $thumbUrl = get_bloginfo('template_directory') . "/images/thumb-browse.jpg";
-            
+                // Get thumbnail URL
+                $thumb = wp_get_attachment_image_src( $attachment->ID, 'browse-thumbnail' );
+                $thumbUrl = $thumb[0];
             endif;
-            // END Get Thumbnail
-        ?>
+        else :
         
-        <a href="<?php the_permalink(); ?>" style="background: url('<?php echo $thumbUrl; ?>') no-repeat 0 0;"></a>
-            
-    </figure>
+            $thumbUrl = get_bloginfo('template_directory') . "/images/thumb-browse.jpg";
+        
+        endif;
+        // END Get Thumbnail
+    ?>
     
-<!-- </article> -->
+    <a href="<?php the_permalink(); ?>" style="background: url('<?php echo $thumbUrl; ?>') no-repeat 0 0;"></a>
+        
+</figure>
