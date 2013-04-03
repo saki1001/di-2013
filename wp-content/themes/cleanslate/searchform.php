dafe0fabd983f9bd8e36c8adce556c45b5d77eae
Saki Sato <sakiisato@gmail.com>
2013-04-03 23:13:55 +0900

    now action url automatically sends it back to browse category page, regardless of starting point

--- wp-content/themes/cleanslate/searchform.php
+++ wp-content/themes/cleanslate/searchform.php
@@ -8,17 +8,20 @@
 ?>
 
 <?php
-    if( is_category() ) :
-        // $category = get_the_category();
-        $category = get_queried_object();
-        $cat_parent = ( $category->category_parent ) ? $category->category_parent : $category->cat_ID;
-        $action_url = get_category_link($cat_parent);
-    else :
-        $action_url = get_bloginfo('home');
-        if( is_search() ) :
-            $action_url .= $_SERVER['REQUEST_URI'];
-        endif;
-    endif;
+    // if( is_category() ) :
+    //     // $category = get_the_category();
+    //     $category = get_queried_object();
+    //     $cat_parent = ( $category->category_parent ) ? $category->category_parent : $category->cat_ID;
+    //     $action_url = get_category_link($cat_parent);
+    // else :
+    //     $action_url = get_bloginfo('home');
+    //     if( is_search() ) :
+    //         $action_url .= $_SERVER['REQUEST_URI'];
+    //     endif;
+    // endif;
+    
+    // Send to Browse category URL
+    $action_url = get_category_link('2');
 ?>
 
 <form role="search" method="get" id="searchform" action="<?php echo $action_url; ?>">
