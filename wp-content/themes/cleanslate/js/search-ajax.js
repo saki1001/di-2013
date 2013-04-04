b2c7ec60e09c00c93ec6ca2d6d713adaef475808
Saki Sato <sakiisato@gmail.com>
2013-04-04 18:10:00 +0900

    took out commented out function, replaced double quotes with single quotes

--- wp-content/themes/cleanslate/js/search-ajax.js
+++ wp-content/themes/cleanslate/js/search-ajax.js
@@ -2,32 +2,12 @@ var $j = jQuery.noConflict();
 
 $j(document).ready(function() {
     
-    // var submitSearch = function(e) {
-    //     e.preventDefault();
-    //     var search_val = $j("#s").val(); 
-    //     
-    //     $j("#searchsubmit").unbind('click', submitSearch);
-    //     
-    //     $j.post(search.php,{search_string:search_val},function(data){
-    //         
-    //         if(data.length>0){
-    //             $j('.browse-posts').html(data);
-    //             console.log(search_val);
-    //         }
-    //     });
-    //     
-    //     $j("#searchsubmit").bind('click', submitSearch);
-    // };
-    // 
-    // 
-    
-    
     // Bind the submit event for your form
     var submitSearch = function(e) {
         
         if (e.which == 13) {
             
-            $j("#s").unbind('keypress', submitSearch);
+            $j('#s').unbind('keypress', submitSearch);
             
             // Get the search term
             var term = $j('#s').val();
@@ -47,15 +27,14 @@ $j(document).ready(function() {
                     if(data.length>0){
                         $j('.browse-posts').html(data);
                     }
-                // console.log(data);
             });
             
-            $j("#s").bind('keypress', submitSearch);
+            $j('#s').bind('keypress', submitSearch);
             
             return false;
         }
         
     };
     
-    $j("#s").bind('keypress', submitSearch);
+    $j('#s').bind('keypress', submitSearch);
 });
