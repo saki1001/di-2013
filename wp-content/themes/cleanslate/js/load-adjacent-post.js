0cf4028a62fd316d40b09a44904ac283acfbb7cc
Saki Sato <sakiisato@gmail.com>
2013-04-06 02:45:51 +0900

    moved fade in fade out control to this file for loading homepage posts, had to use set timeout functions to the animation works in sync with the content loading

--- wp-content/themes/cleanslate/js/load-adjacent-post.js
+++ wp-content/themes/cleanslate/js/load-adjacent-post.js
@@ -1,8 +1,9 @@
-var $j = jQuery.noConflict();
-
 $j(document).ready(function() {
     
     var ajaxRequest = function(postDate) {
+        var date = new Date();
+        var requestStart = date.getTime();
+        
         $j.ajax({
             type: 'GET',
             url: 'wp-content/themes/cleanslate/php/get-adjacent-post.php',
@@ -10,12 +11,24 @@ $j(document).ready(function() {
                 post_date : postDate
             },
             dataType: 'html',
-            beforeSend: function(){
-            },  
             success: function(data){
+                var date = new Date();
+                var requestFinish = date.getTime();
+                var timeSpent = requestFinish - requestStart;
                 
-                $j('#content').html(data);
+                var loadData = function() {
+                    $j('#content').html(data);
+                };
                 
+                // Dont Fade In until ready to load data
+                if ( timeSpent >= 500 ) {
+                    loadData();
+                    fadeInPage();
+                } else {
+                    var timeLeft = 500 - timeSpent;
+                    setTimeout(loadData, timeLeft);
+                    setTimeout(fadeInPage, timeLeft);
+                }
             },  
             error: function(jqXHR, textStatus, errorThrown) {
                 alert(jqXHR + ' :: ' + textStatus + ' :: ' + errorThrown);
@@ -25,15 +38,20 @@ $j(document).ready(function() {
     };
     
     var getAdjacentPost = function() {
+        $j(this).unbind('click', getAdjacentPost);
         
-        $j('this').unbind('click', getAdjacentPost);
+        // Fade Out animation
+        var redirect = false;
+        var turl = $j(this).attr('href');
+        fadeOutPage(redirect, turl);
         
+        // Make ajax request, includes Fade In
         var postId = $j(this).attr('data-id');
         var postDate = $j(this).attr('data-post-date');
+        setTimeout(ajaxRequest(postDate), 1000);
         
-        ajaxRequest(postDate);
-        
-        $j('this').live('click', getAdjacentPost);
+        // Rebind click handler
+        $j(this).live('click', getAdjacentPost);
         
         return false;
     };
