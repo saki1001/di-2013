cc0c004954bf92b0cabaf8d4fe518d8c1d94bd42
Saki Sato <sakiisato@gmail.com>
2013-04-06 02:44:51 +0900

    moved noconflict to main.js file, which then applies it to all following scripts

--- wp-content/themes/cleanslate/js/label-last-article.js
+++ wp-content/themes/cleanslate/js/label-last-article.js
@@ -2,9 +2,6 @@
 // var $j = jQuery.noConflict();
 // $j.getScript("http://localhost:8888/dutch-innovation/wp-content/themes/cleanslate/js/label-last-article.js", function(data, textStatus, jqxhr) {});
 
-
-var $j = jQuery.noConflict();
-
 var labelLastArticle = function() {
     $j('#articles article.last').removeClass('last');
     $j('#articles article').last().addClass('last');
