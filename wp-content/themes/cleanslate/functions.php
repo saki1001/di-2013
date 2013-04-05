2ef1d7fdbac77a6c4a2315f04c89616bfd15f0b5
Saki Sato <sakiisato@gmail.com>
2013-04-06 02:39:37 +0900

    changed browse thumbnail size to accommodate mobile, updated option to not add links to images inserted into posts

--- wp-content/themes/cleanslate/functions.php
+++ wp-content/themes/cleanslate/functions.php
@@ -1,28 +1,16 @@
 <?php
 /**
- * Toolbox functions and definitions
- *
- * Sets up the theme and provides some helper functions. Some helper functions
- * are used in the theme as custom template tags. Others are attached to action and
- * filter hooks in WordPress to change core functionality.
- *
- * When using a child theme (see http://codex.wordpress.org/Theme_Development and
- * http://codex.wordpress.org/Child_Themes), you can override certain functions
- * (those wrapped in a function_exists() call) by defining them first in your child theme's
- * functions.php file. The child theme's functions.php file is included before the parent
- * theme's file, so the child theme functions would be used.
- *
- * Functions that are not pluggable (not wrapped in function_exists()) are instead attached
- * to a filter or action hook. The hook can be removed by using remove_action() or
- * remove_filter() and you can attach your own function to the hook.
- *
- * For more information on hooks, actions, and filters, see http://codex.wordpress.org/Plugin_API.
+ * Cleanslate functions and definitions
  *
  * @package CleanSlate
  * @since CleanSlate 0.1
  */
 
-// BELOW IS ADDED BY SAKI
+function register_jquery() {
+    wp_enqueue_script( 'jquery' );
+}
+add_action('wp_enqueue_scripts', 'register_jquery');
+
 function get_first_attachment() {
     global $post;
 
@@ -88,26 +76,12 @@ function get_category_tags($args) {
 add_theme_support( 'post-thumbnails' );
 
 // Adding Custom Thumbnail Size for Browse page
-add_image_size( 'browse-thumbnail', 222, 222, true );
+add_image_size( 'browse-thumbnail', 392, 392, true );
 add_image_size( 'promo-thumbnail', 335, 335, true );
 
 
 // Alter the Loop for homepage
 function most_recent_post( $query ) {
-    
-    // $current_year = date('Y', current_time('timestamp'));
-    // $current_month = date('m', current_time('timestamp'));
-    // $current_day = date('j', current_time('timestamp'));
-    // 
-    //         $args = array(
-    //             'cat'      => 2,
-    //             'year'     => $current_year,
-    //             'monthnum' => $current_month,
-    //             'day' => $current_day,
-    //             'posts_per_page' => '1',
-    //             'order'    => 'ASC'
-    //         );
-    
     if ( $query->is_home() && $query->is_main_query() ) :
             $query->set('posts_per_page', '1');
             $query->set('cat', '2');
@@ -137,12 +111,8 @@ function my_post_class($classes){
     return $classes;
 }
 
-
-function register_jquery() {  
-    wp_enqueue_script( 'jquery' );  
-}       
-add_action('wp_enqueue_scripts', 'register_jquery');
- 
+// Prevent from adding link to inserted imgaes
+update_option('image_default_link_type','none');
 
 /**
  * This theme was built with PHP, Semantic HTML, CSS, love, and a Toolbox.
