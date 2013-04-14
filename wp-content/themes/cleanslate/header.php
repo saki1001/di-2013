07095733a5a7adefcb8aacced2214c603ec6503f
Saki Sato <sakiisato@gmail.com>
2013-04-15 00:06:18 +0900

    removed search-ajax.js and added form.js, added a span for the logo text

--- wp-content/themes/cleanslate/header.php
+++ wp-content/themes/cleanslate/header.php
@@ -47,7 +47,7 @@
         
         <?php /*Custom JS Files*/ ?>
             <script src="<?php echo get_template_directory_uri(); ?>/js/main.js" type="text/javascript"></script>
-            <script src="<?php echo get_template_directory_uri(); ?>/js/search-ajax.js" type="text/javascript"></script>
+            <script src="<?php echo get_template_directory_uri(); ?>/js/forms.js" type="text/javascript"></script>
         <?php
             if( is_home() ) {
         ?>
@@ -66,7 +66,7 @@
         <header id="branding" role="banner">
             <div id="logo">
                 <h1 id="site-title">
-                    <a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">Dutch Innovation</a>
+                    <a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><span>Dutch Innovation</span></a>
                 </h1>
             </div>
             
